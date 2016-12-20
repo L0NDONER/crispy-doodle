a40d690f35d872e7b6080205d2f8144efe175b63
L0NDONER <oflaherty@gmail.com>
2016-12-20 18:48:37 +0000

    This is a Python script to manualy unfollow Twitter unfollowers.

--- twitter.py
+++ twitter.py
@@ -1,29 +1,29 @@
 #! /usr/bin/python
 import tweepy
-from datetime import datetime
+import time
 from keys import keys
-
+ 
 SCREEN_NAME = keys['screen_name']
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
-
+ 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-
-if(api.verify_credentials):
-    print 'We successfully logged in'
-    print datetime.now().strftime("%d-%m-%y %H:%M:%S")
-
-else:
-    print 'You Failed To Login'
-    print datetime.now().strftime("%d-%m-%y %H:%M:%S")
-
+   
 followers = api.followers_ids(SCREEN_NAME)
 friends = api.friends_ids(SCREEN_NAME)
 
+if(api.verify_credentials):
+	try:
+		print("We successfully Logged In To Twitter:", SCREEN_NAME)
+	except:
+		print 'Uh-Oh something went wrong'
+	else:
+		print(time.strftime('%a - %B - %d - %Y - %H:%M:%S'))
+ 
 for f in friends:
     if f not in followers:
         print "Unfollow {0}?".format(api.get_user(f).screen_name)
